d23f9c8050681b3b20d034f3d9051ea08b48cb56
ppfsa1_ISCTE <ppfsa1@iscte-iul.pt>
2023-11-25 15:45:24 +0000

    Update "HorarioLoader.java"
    Esta classe corresponde aos pontos 1. e 2. do trabalho, já foi entregue. Este commit serve para correção de alguns bugs

--- ES2023LETIGrupoJ/src/carregamento_de_horário/HorarioLoader.java
+++ ES2023LETIGrupoJ/src/carregamento_de_horário/HorarioLoader.java
@@ -1,4 +1,5 @@
 package carregamento_de_horário;
+
 import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,13 +19,10 @@ import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 import com.opencsv.exceptions.CsvException;
 
-
 //Este package contem implementado os pontos 1. e 2. do projeto.
 //O objetivo é o carregamento de um horário a partir do ficheiro do horário CSV
 
-
 public class HorarioLoader {
-	
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("A Minha Aplicação");
@@ -58,7 +56,7 @@ public class HorarioLoader {
 					Scanner scanner = new Scanner(System.in);
 
 					System.out.print(
-							"Indique na consola a diretoria onde contem do arquivo CSV que pretende abrir, na sua máquina local seguido de enter: ");
+							"Indique na consola a diretoria onde contem o arquivo CSV do horário (horario-exemplo.csv): ");
 					String csvFilePath = scanner.nextLine();
 
 					String htmlContent = loadHorarioFromCSV(csvFilePath);
@@ -66,7 +64,7 @@ public class HorarioLoader {
 					// Salvar o conteúdo HTML em um arquivo
 
 					System.out.print(
-							"Indique na consola a diretoria onde contem do arquivo 'SalasDeAulaPorTiposDeSala.html' na sua máquina local: ");
+							"Indique na consola a diretoria onde contem do arquivo (SalasDeAulaPorTiposDeSala.html): ");
 
 					String htmlFilePath = scanner.nextLine();
 					saveHTMLToFile(htmlFilePath, htmlContent);
@@ -98,64 +96,66 @@ public class HorarioLoader {
 		List<List<String>> records = new ArrayList<>();
 		try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
 			String line;
+			String headerLine = br.readLine();
+			String[] headerColumns = headerLine.split(";");
 			while ((line = br.readLine()) != null) {
 				String[] values = line.split(";");
 				records.add(Arrays.asList(values));
 			}
-		}
 
-		StringBuilder htmlContent = new StringBuilder();
-		htmlContent.append("<html lang='en' xmlns='http://www.w3.org/1999/xhtml'>\n" + "	<head>\n"
-				+ "		<meta charset='utf-8' />\n"
-				+ "		<link href='https://unpkg.com/tabulator-tables@4.8.4/dist/css/tabulator.min.css' rel='stylesheet'>\n"
-				+ "		<script type='text/javascript' src='https://unpkg.com/tabulator-tables@4.8.4/dist/js/tabulator.min.js'></script>\n"
-				+ "	</head>\n" + "	<body>\n" + "		<H1>Tipos de Salas de Aula</H1>	\n"
-				+ "		<div id='example-table'></div>\n" + "\n" + "		<script type='text/javascript'>\n" + "\n"
-				+ "			var tabledata = [ \n");
+			StringBuilder htmlContent = new StringBuilder();
+			htmlContent.append("<html lang='en' xmlns='http://www.w3.org/1999/xhtml'>\n" + "	<head>\n"
+					+ "		<meta charset='utf-8' />\n"
+					+ "		<link href='https://unpkg.com/tabulator-tables@4.8.4/dist/css/tabulator.min.css' rel='stylesheet'>\n"
+					+ "		<script type='text/javascript' src='https://unpkg.com/tabulator-tables@4.8.4/dist/js/tabulator.min.js'></script>\n"
+					+ "	</head>\n" + "	<body>\n" + "		<H1>Tipos de Salas de Aula</H1>	\n"
+					+ "		<div id='example-table'></div>\n" + "\n" + "		<script type='text/javascript'>\n"
+					+ "\n" + "			var tabledata = [ \n");
 
-		for (Iterator rowIterator = records.iterator(); rowIterator.hasNext();) {
-			List<String> row = (List<String>) rowIterator.next();
-			htmlContent.append("\t{");
+			for (Iterator rowIterator = records.iterator(); rowIterator.hasNext();) {
+				List<String> row = (List<String>) rowIterator.next();
+				htmlContent.append("\t{");
 
 // Adiciona cada coluna ao objeto JavaScript
-			Iterator<String> columnIterator = row.iterator();
-			while (columnIterator.hasNext()) {
-				String column = columnIterator.next();
+				Iterator<String> columnIterator = row.iterator();
+				int columnIndex = 0;
+
+				while (columnIterator.hasNext()) {
+					String column = columnIterator.next();
 
 // Extrai o nome da coluna (por exemplo, colunaCurso) da lista de registros
-				String columnName = "coluna" + row.indexOf(column);
+					String columnName = "coluna" + columnIndex;
 
 // Adiciona a coluna ao objeto JavaScript
-				htmlContent.append(columnName).append(": '").append(column).append("',");
+					htmlContent.append(columnName).append(": '").append(column).append("',");
 
-				if (columnIterator.hasNext()) {
-					htmlContent.append("\n\t");
+					if (columnIterator.hasNext()) {
+						htmlContent.append("\n\t");
+					}
+
+					columnIndex++;
 				}
+
+				htmlContent.append("},\n");
+			}
+			htmlContent.append("];\n" + "			var table = new Tabulator('#example-table', {\n"
+					+ "				data:tabledata,\n" + "				layout:'fitDatafill',\n"
+					+ "				pagination:'local',\n" + "				paginationSize:10,\n"
+					+ "				paginationSizeSelector:[5, 10, 20, 40],\n" + "				movableColumns:true,\n"
+					+ "				paginationCounter:'rows',\n"
+					+ "				initialSort:[{column:'building',dir:'asc'},],\n" + "				columns:[\n");
+
+			// Ajuste: começamos a partir do segundo elemento
+			for (int columnIndex = 0; columnIndex < headerColumns.length; columnIndex++) {
+				htmlContent.append("					{title:'").append(headerColumns[columnIndex])
+						.append("', field:'coluna").append(columnIndex).append("', headerFilter:'input'},\n");
 			}
 
-			htmlContent.append("},\n");
-		}
-		htmlContent.append("];\n" + "			\n" + "			var table = new Tabulator('#example-table', {\n"
-				+ "				data:tabledata,\n" + "				layout:'fitDatafill',\n"
-				+ "				pagination:'local',\n" + "				paginationSize:10,\n"
-				+ "				paginationSizeSelector:[5, 10, 20, 40],\n" + "				movableColumns:true,\n"
-				+ "				paginationCounter:'rows',\n"
-				+ "				initialSort:[{column:'building',dir:'asc'},],\n" + "				columns:[\n"
-				+ "					{title:'Curso', field:'coluna0', headerFilter:'input'},\n"
-				+ "					{title:'Unidade Curricular', field:'coluna1', headerFilter:'input'},\n"
-				+ "					{title:'Turno', field:'coluna2', headerFilter:'input'},\n"
-				+ "					{title:'Turma', field:'coluna3', headerFilter:'input'},\n"
-				+ "					{title:'Inscritos no turno', field:'coluna4', headerFilter:'input'},\n"
-				+ "					{title:'Dia da semana', field:'coluna5', headerFilter:'input'},\n"
-				+ "					{title:'Hora início da aula', field:'coluna6', headerFilter:'input'},\n"
-				+ "					{title:'Hora fim da aula', field:'coluna7', headerFilter:'input'},\n"
-				+ "					{title:'Data da aula', field:'coluna8', headerFilter:'input'},\n"
-				+ "					{title:'Características da sala pedida para a aula', field:'coluna9', headerFilter:'input'},\n"
-				+ "					{title:'Sala atribuída à aula', field:'coluna10', headerFilter:'input'},\n"
-				+ "				],\n" + "			});\n" + "		</script>\n" + "		\n" + "	</body>\n"
-				+ "</html>");
+			htmlContent.append(
+					"				],\n" + "			});\n" + "		</script>\n" + "	</body>\n" + "</html>");
 
-		return htmlContent.toString();
+			return htmlContent.toString();
+		}
 	}
 
 	public static void saveHTMLToFile(String htmlFilePath, String htmlContent) throws IOException {

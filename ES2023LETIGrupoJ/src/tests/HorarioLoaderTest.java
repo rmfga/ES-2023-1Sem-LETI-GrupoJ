d441c1b2e70b25e80d50160526c4fe4d69befacf
vmpts <vmpts@iscte-iul.pt>
2023-12-09 16:23:18 +0000

    Update HorarioLoaderTest.java
    Adicionar os testes que já estavam nesta classe mas noutro package e fazer alterações (adicionar e remover) alguns testes

--- ES2023LETIGrupoJ/src/tests/HorarioLoaderTest.java
+++ ES2023LETIGrupoJ/src/tests/HorarioLoaderTest.java
@@ -1,34 +1,140 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.opencsv.exceptions.CsvException;
 
-class HorarioLoaderTest {
+import carregamento_de_horário.HorarioLoader;
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+public class HorarioLoaderTest {
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+    private static final String CSV_FILE = "teste-horario.csv";
+    private static final String HTML_FILE = "teste-HTML.html";
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Before
+    public void setUp() throws IOException {
+    	//cria ficheiro CSV com cabeçalho e uma linha de exemplo
+        createCSVFile(CSV_FILE);
+    }
 
+    @After
+    public void tearDown() {
+    	//elimina ficheiros criados para os testes
+        deleteFile(CSV_FILE);
+        deleteFile(HTML_FILE);
+    }
+
+    @Test
+    public void testLoadHorarioFromCSVAndCompareWithCSVFile() throws IOException, CsvException {
+    	//Verifica se o Horario carregado contém as informações necessárias
+        String actualHtml = HorarioLoader.loadHorarioFromCSV(CSV_FILE);
+        assertTrue(actualHtml.contains("<H1>Tipos de Salas de Aula</H1>"));
+        assertTrue(actualHtml.contains("var table = new Tabulator('#example-table', {"));
+    }
+
+    @Test
+    public void testSaveHTMLToFileAndCompareWithHTMLFile() throws IOException {
+    	//Testa se, ao guardar o conteudo num ficheiro, o ficheiro tem o conteudo esperado
+        String expectedHtml = "<html><body><p>Linha de teste</p></body></html>";
+        HorarioLoader.saveHTMLToFile(HTML_FILE, expectedHtml);
+        String actualHtml = fileAsString(HTML_FILE);
+        assertEquals(expectedHtml, actualHtml);
+    }
+
+    @Test
+    //Tenta dar load a um ficheiro num caminho nulo
+    public void testLoadHorarioFromCSVWithNullCsvFilePath() throws IOException, CsvException {
+        assertNull(HorarioLoader.loadHorarioFromCSV(null));
+    }
+
+    @Test
+    //Tenta dar load a um ficheiro num caminho vazio
+    public void testLoadHorarioFromCSVWithEmptyCsvFilePath() throws IOException, CsvException {
+        assertNull(HorarioLoader.loadHorarioFromCSV(""));
+    }
+
+    @Test
+    public void testSaveHTMLToFileWithNullHtmlContent() throws IOException {
+    	//Tenta guardar conteudo nulo num ficheiro
+        HorarioLoader.saveHTMLToFile("teste.html", null);
+        assertFalse(new File("teste.html").exists());
+    }
+
+    @Test
+    public void testSaveHTMLToFileWithEmptyHtmlContent() throws IOException {
+    	//Tenta guardar conteudo vazio num ficheiro
+        HorarioLoader.saveHTMLToFile("teste.html", "");
+        assertFalse(new File("teste.html").exists());
+    }
+    
+    @Test
+    public void testLoadRegistrosFromCSV() throws IOException, CsvException {
+    	//verifica se é permitido carregar registros de ficheiro de caminho vazio ou nulo
+    	assertNull(HorarioLoader.loadRegistrosFromCSV(""));
+    	assertNull(HorarioLoader.loadRegistrosFromCSV(null));
+    	
+    	List<List<String>> records3 = HorarioLoader.loadRegistrosFromCSV(CSV_FILE);
+    	//confirma que o numero de linhas de registos esperados vão ao encontro com o esperado
+        assertEquals(1, records3.size()); 
+        
+        // Compara os registos carregados do ficheiro com os esperados
+        assertEquals("Engenharia", records3.get(0).get(0));
+        assertEquals("100", records3.get(0).get(1));
+        assertEquals("Dr. Silva", records3.get(0).get(2));
+    }
+
+    @Test
+    public void testLerNomesColunasDoCSV() throws IOException {
+    	//Testa se é possivel ler nomes de colunas de um ficheiro de um
+    	//caminho nulo ou vazio
+    	assertNull(HorarioLoader.lerNomesColunasDoCSV(""));
+    	assertNull(HorarioLoader.lerNomesColunasDoCSV(null));
+        List<String> headerColumns = HorarioLoader.lerNomesColunasDoCSV(CSV_FILE);
+        assertNotNull(headerColumns);
+        
+        //Confirma a quantidade de colunas
+        assertEquals(3, headerColumns.size()); 
+        
+        //Confirma o nome das colunas
+        assertEquals("Nome do Curso", headerColumns.get(0));
+        assertEquals("Número de Alunos", headerColumns.get(1));
+        assertEquals("Professor", headerColumns.get(2));
+    }
+
+    private void createCSVFile(String filePath) throws IOException {
+        try (FileWriter writer = new FileWriter(filePath)) {
+            // Adiciona um cabeçalho ao arquivo CSV
+            writer.write("Nome do Curso;Número de Alunos;Professor\n");
+            // Adiciona uma linha de exemplo
+            writer.write("Engenharia;100;Dr. Silva\n");
+        }
+    }
+
+    private void deleteFile(String filePath) {
+        File file = new File(filePath);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    private String fileAsString(String filePath) throws IOException {
+        Path path = Paths.get(filePath);
+        return Files.readString(path);
+    }
 }

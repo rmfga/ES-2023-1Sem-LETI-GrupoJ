f617fa1baa9364a6cf7f9d3624b441b7c47a6cfa
ppfsa1_ISCTE <ppfsa1@iscte-iul.pt>
2023-12-09 23:44:12 +0000

    Create SaveFiles.java
    Classe que serve para guardar os ficheiros. Como o nossa classe GUI já tava a ficar demasiado preenchida optamos por criar uma classe que serve apenas para salvar os ficheiros.

---
+++ ES2023LETIGrupoJ/src/carregamento_de_horário/SaveFiles.java
@@ -0,0 +1,59 @@
+package carregamento_de_horário;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import application.GUI;
+
+public class SaveFiles {
+
+	public static void salvarArquivoHTML(String htmlContent) {
+		try {
+			JFileChooser fileChooser = new JFileChooser();
+			
+			if (GUI.getDiretorioEscolhido() != null) {
+                fileChooser.setCurrentDirectory(new File(GUI.getDiretorioEscolhido()));
+            }
+
+
+			// Configurar o local para salvar o arquivo HTML
+			fileChooser.setDialogTitle("Selecione o local para salvar o arquivo HTML");
+			fileChooser.setFileFilter(new FileNameExtensionFilter("Arquivos HTML", "html"));
+			fileChooser.setSelectedFile(new File("SalasDeAulaPorTiposDeSala.html"));
+
+			int result = fileChooser.showSaveDialog(null);
+
+			if (result == JFileChooser.APPROVE_OPTION) {
+				// Obter o caminho completo do arquivo HTML
+				String htmlFilePath = fileChooser.getSelectedFile().getAbsolutePath();
+
+				// Salvar o conteúdo HTML no arquivo
+				saveHTMLToFile(htmlFilePath, htmlContent);
+				htmlFilePath = htmlFilePath.replace("\\", "/");
+
+				// Abrir o arquivo HTML no navegador padrão
+				Desktop desk = Desktop.getDesktop();
+				desk.browse(new java.net.URI("file://" + htmlFilePath));
+			}
+		} catch (IOException | URISyntaxException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static void saveHTMLToFile(String htmlFilePath, String htmlContent) throws IOException {
+		if (htmlContent == null || htmlContent.isEmpty()) {
+			System.out.println("O conteúdo do ficheiro HTML não pode ser nulo ou vazio.");
+			return;
+		}
+		try (FileWriter writer = new FileWriter(htmlFilePath)) {
+			writer.write(htmlContent);
+		}
+	}
+
+}

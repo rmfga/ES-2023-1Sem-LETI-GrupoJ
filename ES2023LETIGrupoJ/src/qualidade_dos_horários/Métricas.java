d093c14de34b08c1fa0b069d17b238c550d5f4ad
ppfsa1_ISCTE <ppfsa1@iscte-iul.pt>
2023-12-10 08:33:16 +0000

    Update Métricas.java
    Remoção do método saveHTMLToFile, que agora está implementado na classe SaveFiles.

--- ES2023LETIGrupoJ/src/qualidade_dos_horários/Métricas.java
+++ ES2023LETIGrupoJ/src/qualidade_dos_horários/Métricas.java
@@ -143,78 +143,73 @@ public class Métricas {
 		List<List<String>> horario = horarioISCTE.getHorario();
 		final int[] count = { 0 }; // Contador de Salas com caracteristicas diferentes
 
-			// Filtrar as linhas com a condição desejada
-			List<List<String>> filteredRecords = horario.stream()
-					.filter(row -> row.size() >= 11
-							&& (("Sala de Aulas normal".equals(row.get(9)) && row.get(10).startsWith("Auditório"))
-									|| ("Laboratório de Informática".equals(row.get(9))
-											&& row.get(10).startsWith("Auditório"))))
-					.peek(row -> count[0]++) // Incrementa o contador para cada linha que passa pelo filtro
-					.collect(Collectors.toList());
+		// Filtrar as linhas com a condição desejada
+		List<List<String>> filteredRecords = horario.stream().filter(row -> row.size() >= 11
+				&& (("Sala de Aulas normal".equals(row.get(9)) && row.get(10).startsWith("Auditório"))
+						|| ("Laboratório de Informática".equals(row.get(9)) && row.get(10).startsWith("Auditório"))))
+				.peek(row -> count[0]++) // Incrementa o contador para cada linha que passa pelo filtro
+				.collect(Collectors.toList());
 
-			StringBuilder htmlContent = new StringBuilder();
-			htmlContent.append("<html lang='en' xmlns='http://www.w3.org/1999/xhtml'>\n" + "	<head>\n"
-					+ "		<meta charset='utf-8' />\n"
-					+ "		<link href='https://unpkg.com/tabulator-tables@4.8.4/dist/css/tabulator.min.css' rel='stylesheet'>\n"
-					+ "		<script type='text/javascript' src='https://unpkg.com/tabulator-tables@4.8.4/dist/js/tabulator.min.js'></script>\n"
-					+ "	</head>\n" + "	<body>\n" + "		<H1>Cálculo das Métricas</H1>	\n"
-					+ "		<div id='example-table'></div>\n" + "\n" + "		<script type='text/javascript'>\n"
-					+ "\n" + "			var tabledata = [ \n");
+		StringBuilder htmlContent = new StringBuilder();
+		htmlContent.append("<html lang='en' xmlns='http://www.w3.org/1999/xhtml'>\n" + "	<head>\n"
+				+ "		<meta charset='utf-8' />\n"
+				+ "		<link href='https://unpkg.com/tabulator-tables@4.8.4/dist/css/tabulator.min.css' rel='stylesheet'>\n"
+				+ "		<script type='text/javascript' src='https://unpkg.com/tabulator-tables@4.8.4/dist/js/tabulator.min.js'></script>\n"
+				+ "	</head>\n" + "	<body>\n" + "		<H1>Cálculo das Métricas</H1>	\n"
+				+ "		<div id='example-table'></div>\n" + "\n" + "		<script type='text/javascript'>\n" + "\n"
+				+ "			var tabledata = [ \n");
 
-			for (Iterator rowIterator = filteredRecords.iterator(); rowIterator.hasNext();) {
-				List<String> row = (List<String>) rowIterator.next();
-				htmlContent.append("\t{");
+		for (Iterator rowIterator = filteredRecords.iterator(); rowIterator.hasNext();) {
+			List<String> row = (List<String>) rowIterator.next();
+			htmlContent.append("\t{");
 
-				// Adiciona cada coluna ao objeto JavaScript
-				Iterator<String> columnIterator = row.iterator();
-				int columnIndex = 0;
+			// Adiciona cada coluna ao objeto JavaScript
+			Iterator<String> columnIterator = row.iterator();
+			int columnIndex = 0;
 
-				while (columnIterator.hasNext()) {
-					String column = columnIterator.next();
+			while (columnIterator.hasNext()) {
+				String column = columnIterator.next();
 
-					// Extrai o nome da coluna (por exemplo, colunaCurso) da lista de registros
-					String columnName = "coluna" + columnIndex;
+				// Extrai o nome da coluna (por exemplo, colunaCurso) da lista de registros
+				String columnName = "coluna" + columnIndex;
 
-					// Adiciona a coluna ao objeto JavaScript
-					htmlContent.append(columnName).append(": '").append(column).append("',");
+				// Adiciona a coluna ao objeto JavaScript
+				htmlContent.append(columnName).append(": '").append(column).append("',");
 
-					if (columnIterator.hasNext()) {
-						htmlContent.append("\n\t");
-					}
-
-					columnIndex++;
+				if (columnIterator.hasNext()) {
+					htmlContent.append("\n\t");
 				}
 
-				htmlContent.append("},\n");
-			}
-			htmlContent.append("];\n" + "			var table = new Tabulator('#example-table', {\n"
-					+ "				data:tabledata,\n" + "				layout:'fitDatafill',\n"
-					+ "				pagination:'local',\n" + "				paginationSize:10,\n"
-					+ "				paginationSizeSelector:[5, 10, 20, 40],\n" + "				movableColumns:true,\n"
-					+ "				paginationCounter:'rows',\n"
-					+ "				initialSort:[{column:'building',dir:'asc'},],\n" + "				columns:[\n");
-
-			// Ajuste: começamos a partir do segundo elemento
-			for (int columnIndex = 0; columnIndex < horarioISCTE.getHeaderColumns().size(); columnIndex++) {
-				String columnName = horarioISCTE.getHeaderColumns().get(columnIndex);
-				int columnIndexInArray = horarioISCTE.getColumnIndex(columnName);
-
-				
-				htmlContent.append("					{title:'").append(columnName)
-						.append("', field:'coluna").append(columnIndexInArray).append("', headerFilter:'input'},\n");
+				columnIndex++;
 			}
 
-			htmlContent.append(
-					"				],\n" + "			});\n" + "		</script>\n" + "	</body>\n" + "</html>");
+			htmlContent.append("},\n");
+		}
+		htmlContent.append("];\n" + "			var table = new Tabulator('#example-table', {\n"
+				+ "				data:tabledata,\n" + "				layout:'fitDatafill',\n"
+				+ "				pagination:'local',\n" + "				paginationSize:10,\n"
+				+ "				paginationSizeSelector:[5, 10, 20, 40],\n" + "				movableColumns:true,\n"
+				+ "				paginationCounter:'rows',\n"
+				+ "				initialSort:[{column:'building',dir:'asc'},],\n" + "				columns:[\n");
 
-			JOptionPane.showMessageDialog(null,
-					"Nº de aulas realizadas em salas que não têm as características solicitadas: " + count[0], "Aviso",
-					JOptionPane.WARNING_MESSAGE);
+		// Ajuste: começamos a partir do segundo elemento
+		for (int columnIndex = 0; columnIndex < horarioISCTE.getHeaderColumns().size(); columnIndex++) {
+			String columnName = horarioISCTE.getHeaderColumns().get(columnIndex);
+			int columnIndexInArray = horarioISCTE.getColumnIndex(columnName);
 
-			return htmlContent.toString();
+			htmlContent.append("					{title:'").append(columnName).append("', field:'coluna")
+					.append(columnIndexInArray).append("', headerFilter:'input'},\n");
 		}
 
-	
+		htmlContent
+				.append("				],\n" + "			});\n" + "		</script>\n" + "	</body>\n" + "</html>");
+
+		JOptionPane.showMessageDialog(null,
+				"Nº de aulas realizadas em salas que não têm as características solicitadas: " + count[0], "Aviso",
+				JOptionPane.WARNING_MESSAGE);
+
+		return htmlContent.toString();
+	}
 
 	////////////////////////////////////////////////////////////////////////////////
 
@@ -228,85 +223,78 @@ public class Métricas {
 		List<List<String>> horario = horarioISCTE.getHorario();
 		final int[] count = { 0 }; // Contador de Aulas sem Sala Atribuida
 
-			// Filtrar as linhas com tamanho igual a 10, ou seja a coluna da sala atribuida
-			// está vazia
-			List<List<String>> filteredRecords = horario.stream().filter(row -> row.size() == 10)
-					.peek(row -> count[0]++) // Incrementa o contador para cada linha que passa pelo filtro
-					.collect(Collectors.toList());
+		// Filtrar as linhas com tamanho igual a 10, ou seja a coluna da sala atribuida
+		// está vazia
+		List<List<String>> filteredRecords = horario.stream().filter(row -> row.size() == 10).peek(row -> count[0]++) // Incrementa
+																														// o
+																														// contador
+																														// para
+																														// cada
+																														// linha
+																														// que
+																														// passa
+																														// pelo
+																														// filtro
+				.collect(Collectors.toList());
 
-			StringBuilder htmlContent = new StringBuilder();
-			htmlContent.append("<html lang='en' xmlns='http://www.w3.org/1999/xhtml'>\n" + "	<head>\n"
-					+ "		<meta charset='utf-8' />\n"
-					+ "		<link href='https://unpkg.com/tabulator-tables@4.8.4/dist/css/tabulator.min.css' rel='stylesheet'>\n"
-					+ "		<script type='text/javascript' src='https://unpkg.com/tabulator-tables@4.8.4/dist/js/tabulator.min.js'></script>\n"
-					+ "	</head>\n" + "	<body>\n" + "		<H1>Cálculo das Métricas</H1>	\n"
-					+ "		<div id='example-table'></div>\n" + "\n" + "		<script type='text/javascript'>\n"
-					+ "\n" + "			var tabledata = [ \n");
+		StringBuilder htmlContent = new StringBuilder();
+		htmlContent.append("<html lang='en' xmlns='http://www.w3.org/1999/xhtml'>\n" + "	<head>\n"
+				+ "		<meta charset='utf-8' />\n"
+				+ "		<link href='https://unpkg.com/tabulator-tables@4.8.4/dist/css/tabulator.min.css' rel='stylesheet'>\n"
+				+ "		<script type='text/javascript' src='https://unpkg.com/tabulator-tables@4.8.4/dist/js/tabulator.min.js'></script>\n"
+				+ "	</head>\n" + "	<body>\n" + "		<H1>Cálculo das Métricas</H1>	\n"
+				+ "		<div id='example-table'></div>\n" + "\n" + "		<script type='text/javascript'>\n" + "\n"
+				+ "			var tabledata = [ \n");
 
-			for (Iterator rowIterator = filteredRecords.iterator(); rowIterator.hasNext();) {
-				List<String> row = (List<String>) rowIterator.next();
-				htmlContent.append("\t{");
+		for (Iterator rowIterator = filteredRecords.iterator(); rowIterator.hasNext();) {
+			List<String> row = (List<String>) rowIterator.next();
+			htmlContent.append("\t{");
 
-				// Adiciona cada coluna ao objeto JavaScript
-				Iterator<String> columnIterator = row.iterator();
-				int columnIndex = 0;
+			// Adiciona cada coluna ao objeto JavaScript
+			Iterator<String> columnIterator = row.iterator();
+			int columnIndex = 0;
 
-				while (columnIterator.hasNext()) {
-					String column = columnIterator.next();
+			while (columnIterator.hasNext()) {
+				String column = columnIterator.next();
 
-					// Extrai o nome da coluna (por exemplo, colunaCurso) da lista de registros
-					String columnName = "coluna" + columnIndex;
+				// Extrai o nome da coluna (por exemplo, colunaCurso) da lista de registros
+				String columnName = "coluna" + columnIndex;
 
-					// Adiciona a coluna ao objeto JavaScript
-					htmlContent.append(columnName).append(": '").append(column).append("',");
+				// Adiciona a coluna ao objeto JavaScript
+				htmlContent.append(columnName).append(": '").append(column).append("',");
 
-					if (columnIterator.hasNext()) {
-						htmlContent.append("\n\t");
-					}
-
-					columnIndex++;
+				if (columnIterator.hasNext()) {
+					htmlContent.append("\n\t");
 				}
 
-				htmlContent.append("},\n");
-			}
-			htmlContent.append("];\n" + "			var table = new Tabulator('#example-table', {\n"
-					+ "				data:tabledata,\n" + "				layout:'fitDatafill',\n"
-					+ "				pagination:'local',\n" + "				paginationSize:10,\n"
-					+ "				paginationSizeSelector:[5, 10, 20, 40],\n" + "				movableColumns:true,\n"
-					+ "				paginationCounter:'rows',\n"
-					+ "				initialSort:[{column:'building',dir:'asc'},],\n" + "				columns:[\n");
-
-			// Ajuste: começamos a partir do segundo elemento
-			for (int columnIndex = 0; columnIndex < 10; columnIndex++) {
-				String columnName = horarioISCTE.getHeaderColumns().get(columnIndex);
-				int columnIndexInArray = horarioISCTE.getColumnIndex(columnName);
-
-				
-				htmlContent.append("					{title:'").append(columnName)
-						.append("', field:'coluna").append(columnIndexInArray).append("', headerFilter:'input'},\n");
+				columnIndex++;
 			}
 
-			htmlContent.append(
-					"				],\n" + "			});\n" + "		</script>\n" + "	</body>\n" + "</html>");
-
-			JOptionPane.showMessageDialog(null, "Número de aulas sem sala atribuída: " + count[0], "Aviso",
-					JOptionPane.WARNING_MESSAGE);
-
-			return htmlContent.toString();
+			htmlContent.append("},\n");
 		}
-	
+		htmlContent.append("];\n" + "			var table = new Tabulator('#example-table', {\n"
+				+ "				data:tabledata,\n" + "				layout:'fitDatafill',\n"
+				+ "				pagination:'local',\n" + "				paginationSize:10,\n"
+				+ "				paginationSizeSelector:[5, 10, 20, 40],\n" + "				movableColumns:true,\n"
+				+ "				paginationCounter:'rows',\n"
+				+ "				initialSort:[{column:'building',dir:'asc'},],\n" + "				columns:[\n");
 
-	////////////////////////////////////////////////////////////////////////////////
+		// Ajuste: começamos a partir do segundo elemento
+		for (int columnIndex = 0; columnIndex < 10; columnIndex++) {
+			String columnName = horarioISCTE.getHeaderColumns().get(columnIndex);
+			int columnIndexInArray = horarioISCTE.getColumnIndex(columnName);
 
-	////////////////////////////////////////////////////////////////////////////////
-
-	public static void saveHTMLToFile(String htmlFilePath, String htmlContent) throws IOException {
-		if (htmlContent == null || htmlContent.isEmpty()) {
-			System.out.println("O conteúdo do ficheiro HTML não pode ser nulo ou vazio.");
-			return;
+			htmlContent.append("					{title:'").append(columnName).append("', field:'coluna")
+					.append(columnIndexInArray).append("', headerFilter:'input'},\n");
 		}
-		try (FileWriter writer = new FileWriter(htmlFilePath)) {
-			writer.write(htmlContent);
-		}
+
+		htmlContent
+				.append("				],\n" + "			});\n" + "		</script>\n" + "	</body>\n" + "</html>");
+
+		JOptionPane.showMessageDialog(null, "Número de aulas sem sala atribuída: " + count[0], "Aviso",
+				JOptionPane.WARNING_MESSAGE);
+
+		return htmlContent.toString();
 	}
+
 }

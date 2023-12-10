0be171bc186e7e4df6809f77d4c09a106ae7d02a
vmpts <vmpts@iscte-iul.pt>
2023-12-10 12:12:56 +0000

    Update AllTests
    Classe que testa todos os testes

--- ES2023LETIGrupoJ/src/tests/AllTests.java
+++ ES2023LETIGrupoJ/src/tests/AllTests.java
@@ -2,10 +2,10 @@ package tests;
 
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
-
+//não tou a fazer do ColumnOrderDialog, GUI e Métricas pq envolve interações com Interface
 @Suite
-@SelectClasses({ ColumnOrderDialogTest.class, Horario_ISCTETest.class, HorarioLoaderTest.class, MapeamentoTest.class,
-		MétricasTest.class, SalaTest.class })
+@SelectClasses({ Horario_ISCTETest.class, HorarioLoaderTest.class, MapeamentoTest.class, ListaSalas_ISCTETest.class,
+		ListaSalasLoaderTest.class, SaveFilesTest.class})
 public class AllTests {
 
 }
